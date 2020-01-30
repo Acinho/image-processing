f9e7cb41bc3f1dd0ce1b2e35889b62550db1bdf5
Aleksandra <kantar.alex@gmail.com>
2020-01-30 21:49:59 +0100

    implemented shape detection

--- image-processing/Form1.Designer.cs
+++ image-processing/Form1.Designer.cs
@@ -28,13 +28,198 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.choosePictureBtn = new System.Windows.Forms.Button();
+            this.brightnessLbl = new System.Windows.Forms.Label();
+            this.contrastLbl = new System.Windows.Forms.Label();
+            this.konturaLbl = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.konturaCmb = new System.Windows.Forms.ComboBox();
+            this.colorLbl = new System.Windows.Forms.Label();
+            this.sizeLbl = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnDetectObjects = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.trbBrightness = new System.Windows.Forms.TrackBar();
+            this.trbContrast = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbBrightness)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbContrast)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(180, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(608, 426);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // choosePictureBtn
+            // 
+            this.choosePictureBtn.Location = new System.Drawing.Point(12, 12);
+            this.choosePictureBtn.Name = "choosePictureBtn";
+            this.choosePictureBtn.Size = new System.Drawing.Size(162, 23);
+            this.choosePictureBtn.TabIndex = 1;
+            this.choosePictureBtn.Text = "Odaberite sliku";
+            this.choosePictureBtn.UseVisualStyleBackColor = true;
+            this.choosePictureBtn.Click += new System.EventHandler(this.choosePictureBtn_Click);
+            // 
+            // brightnessLbl
+            // 
+            this.brightnessLbl.AutoSize = true;
+            this.brightnessLbl.Location = new System.Drawing.Point(9, 47);
+            this.brightnessLbl.Name = "brightnessLbl";
+            this.brightnessLbl.Size = new System.Drawing.Size(56, 13);
+            this.brightnessLbl.TabIndex = 2;
+            this.brightnessLbl.Text = "Brightness";
+            // 
+            // contrastLbl
+            // 
+            this.contrastLbl.AutoSize = true;
+            this.contrastLbl.Location = new System.Drawing.Point(9, 91);
+            this.contrastLbl.Name = "contrastLbl";
+            this.contrastLbl.Size = new System.Drawing.Size(46, 13);
+            this.contrastLbl.TabIndex = 3;
+            this.contrastLbl.Text = "Contrast";
+            // 
+            // konturaLbl
+            // 
+            this.konturaLbl.AutoSize = true;
+            this.konturaLbl.Location = new System.Drawing.Point(9, 177);
+            this.konturaLbl.Name = "konturaLbl";
+            this.konturaLbl.Size = new System.Drawing.Size(44, 13);
+            this.konturaLbl.TabIndex = 4;
+            this.konturaLbl.Text = "Kontura";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // konturaCmb
+            // 
+            this.konturaCmb.FormattingEnabled = true;
+            this.konturaCmb.Location = new System.Drawing.Point(74, 174);
+            this.konturaCmb.Name = "konturaCmb";
+            this.konturaCmb.Size = new System.Drawing.Size(100, 21);
+            this.konturaCmb.TabIndex = 7;
+            this.konturaCmb.SelectedIndexChanged += new System.EventHandler(this.konturaCmb_SelectedIndexChanged);
+            // 
+            // colorLbl
+            // 
+            this.colorLbl.AutoSize = true;
+            this.colorLbl.Location = new System.Drawing.Point(9, 230);
+            this.colorLbl.Name = "colorLbl";
+            this.colorLbl.Size = new System.Drawing.Size(28, 13);
+            this.colorLbl.TabIndex = 10;
+            this.colorLbl.Text = "Boja";
+            // 
+            // sizeLbl
+            // 
+            this.sizeLbl.AutoSize = true;
+            this.sizeLbl.Location = new System.Drawing.Point(9, 277);
+            this.sizeLbl.Name = "sizeLbl";
+            this.sizeLbl.Size = new System.Drawing.Size(44, 13);
+            this.sizeLbl.TabIndex = 11;
+            this.sizeLbl.Text = "Velicina";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(74, 275);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // btnDetectObjects
+            // 
+            this.btnDetectObjects.Location = new System.Drawing.Point(12, 361);
+            this.btnDetectObjects.Name = "btnDetectObjects";
+            this.btnDetectObjects.Size = new System.Drawing.Size(162, 77);
+            this.btnDetectObjects.TabIndex = 14;
+            this.btnDetectObjects.Text = "Detektuj";
+            this.btnDetectObjects.UseVisualStyleBackColor = true;
+            this.btnDetectObjects.Click += new System.EventHandler(this.btnDetectObjects_Click);
+            // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.Color.Red;
+            this.btnColor.Location = new System.Drawing.Point(74, 225);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(100, 23);
+            this.btnColor.TabIndex = 16;
+            this.btnColor.UseVisualStyleBackColor = false;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // trbBrightness
+            // 
+            this.trbBrightness.Location = new System.Drawing.Point(70, 47);
+            this.trbBrightness.Name = "trbBrightness";
+            this.trbBrightness.Size = new System.Drawing.Size(104, 45);
+            this.trbBrightness.TabIndex = 17;
+            this.trbBrightness.Scroll += new System.EventHandler(this.trbBrightness_Scroll);
+            // 
+            // trbContrast
+            // 
+            this.trbContrast.Location = new System.Drawing.Point(70, 91);
+            this.trbContrast.Name = "trbContrast";
+            this.trbContrast.Size = new System.Drawing.Size(104, 45);
+            this.trbContrast.TabIndex = 18;
+            this.trbContrast.Scroll += new System.EventHandler(this.trbContrast_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.trbContrast);
+            this.Controls.Add(this.trbBrightness);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnDetectObjects);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.sizeLbl);
+            this.Controls.Add(this.colorLbl);
+            this.Controls.Add(this.konturaCmb);
+            this.Controls.Add(this.konturaLbl);
+            this.Controls.Add(this.contrastLbl);
+            this.Controls.Add(this.brightnessLbl);
+            this.Controls.Add(this.choosePictureBtn);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Detekcija oblika";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbBrightness)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbContrast)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button choosePictureBtn;
+        private System.Windows.Forms.Label brightnessLbl;
+        private System.Windows.Forms.Label contrastLbl;
+        private System.Windows.Forms.Label konturaLbl;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ComboBox konturaCmb;
+        private System.Windows.Forms.Label colorLbl;
+        private System.Windows.Forms.Label sizeLbl;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btnDetectObjects;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.TrackBar trbBrightness;
+        private System.Windows.Forms.TrackBar trbContrast;
     }
 }
 
--- image-processing/Form1.cs
+++ image-processing/Form1.cs
@@ -8,13 +8,144 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.CvEnum;
+
 namespace image_processing
 {
     public partial class Form1 : Form
     {
+        Image<Bgr, byte> image;
+
+        int brightness;
+        int contrast;
+        string shape = "Pravougaonik";
+        Color color = Color.Red;
+        int size = 0;
+
         public Form1()
         {
             InitializeComponent();
+
+            konturaCmb.DataSource = new string[] { "Pravougaonik", "Krug", "Trougao" };
+            konturaCmb.SelectedItem = shape;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            image = new Image<Bgr, byte>(Properties.Resources.slika);
+            pictureBox.Image = image.ToBitmap();
+        }
+
+        private void choosePictureBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                image = new Image<Bgr, byte>(openFileDialog1.FileName);
+                pictureBox.Image = image.ToBitmap();
+            }
+        }
+
+        private void konturaCmb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.shape = konturaCmb.SelectedItem.ToString();
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = colorDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                btnColor.BackColor = colorDialog1.Color;
+                color = colorDialog1.Color;
+            }
+        }
+
+        private void trbBrightness_Scroll(object sender, EventArgs e)
+        {
+            Bitmap tmpBmp = new Bitmap(image.Width, image.Height);
+            //pictureBox.Image = tmpBmp;
+        }
+
+        private void trbContrast_Scroll(object sender, EventArgs e)
+        {
+            Bitmap tmpBmp = new Bitmap(image.Width, image.Height);
+            //pictureBox.Image = tmpBmp;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            size = (int)numericUpDown1.Value;
+        }
+
+        private void btnDetectObjects_Click(object sender, EventArgs e)
+        {
+            var tmpImage = image.Copy();
+
+            Bgr lowerLimit = new Bgr(0, 0, 0);
+            Bgr upperLimit = new Bgr(color.B, color.G, color.R);
+            Image<Gray, byte> grayImage = tmpImage.InRange(lowerLimit, upperLimit);
+
+            VectorOfVectorOfPoint shapes = new VectorOfVectorOfPoint();
+            CvInvoke.FindContours(grayImage, shapes, new Mat(), RetrType.External, ChainApproxMethod.ChainApproxSimple);
+
+            for (int i = 0; i < shapes.Size; i++)
+            {
+                double shapePerimiter = CvInvoke.ArcLength(shapes[i], true);
+                VectorOfPoint approx = new VectorOfPoint();
+                CvInvoke.ApproxPolyDP(shapes[i], approx, 0.03 * shapePerimiter, true);
+
+                MCvMoments moments = CvInvoke.Moments(shapes[i]);
+                int x = (int)(moments.M10 / moments.M00) - 60;
+                int y = (int)(moments.M01 / moments.M00);
+
+                if (shape.Equals("Pravougaonik") && ShapeFits(shapePerimiter) && approx.Size == 4)
+                {
+                    CvInvoke.PutText(tmpImage, "Pravougaonik", new Point(x, y), FontFace.HersheyTriplex, 0.6, new MCvScalar(0, 0, 0), 1);
+                }
+                else if (shape.Equals("Krug") && ShapeFits(shapePerimiter) && approx.Size > 6)
+                {
+                    CvInvoke.PutText(tmpImage, "Krug", new Point(x, y), FontFace.HersheyTriplex, 0.6, new MCvScalar(0, 0, 0), 1);
+                }
+                else if (shape.Equals("Trougao") && ShapeFits(shapePerimiter) && approx.Size == 3)
+                {
+                    CvInvoke.PutText(tmpImage, "Trougao", new Point(x, y), FontFace.HersheyTriplex, 0.6, new MCvScalar(0, 0, 0), 1);
+                }
+
+                pictureBox.Image = tmpImage.ToBitmap();
+            }
+        }
+
+        private bool ShapeFits(double shapeSize)
+        {
+            if (size == 0)
+                return true;
+            if (size == 1 && shapeSize <= 100.0)
+                return true;
+            if (size == 2 && shapeSize <= 200.0 && shapeSize > 100)
+                return true;
+            if (size == 3 && shapeSize <= 300.0 && shapeSize > 200)
+                return true;
+            if (size == 4 && shapeSize <= 400.0 && shapeSize > 300)
+                return true;
+            if (size == 5 && shapeSize <= 500.0 && shapeSize > 400)
+                return true;
+            if (size == 6 && shapeSize <= 600.0 && shapeSize > 500)
+                return true;
+            if (size == 7 && shapeSize <= 700.0 && shapeSize > 600)
+                return true;
+            if (size == 8 && shapeSize <= 800.0 && shapeSize > 700)
+                return true;
+            if (size == 9 && shapeSize <= 900.0 && shapeSize > 800)
+                return true;
+            if (size == 10 && shapeSize <= 1000.0 && shapeSize > 900)
+                return true;
+            return false;
         }
     }
+
 }
